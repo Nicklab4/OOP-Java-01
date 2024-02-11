c213b26a5517a462c84b6c6b14eee8e2a7b288ac
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-02-11 18:05:29 +0500

    Домашнее задание 03 v.01

---
+++ src/drugstore/components/impl/Azitromite.java
@@ -0,0 +1,9 @@
+package drugstore.components.impl;
+
+import drugstore.components.Component;
+
+public class Azitromite extends Component {
+    public Azitromite(String title, double weight, int power) {
+        super(title, weight, power);
+    }
+}
---
+++ src/drugstore/components/impl/Water.java
@@ -0,0 +1,9 @@
+package drugstore.components.impl;
+
+import drugstore.components.Component;
+
+public class Water extends Component {
+    public Water(String title, double weight, int power) {
+        super(title, weight, power);
+    }
+}
